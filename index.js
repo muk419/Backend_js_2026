6ae4670adbe9ba318db2799d4c02e91557757dab
Mukesh Yadav <mukesh881yadav@gmail.com>
2026-01-14 17:34:43 +0530

    add  json

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT=process.env.PORT
+
+const githubRepo={
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://hitesh.ai",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 117,
+  "public_gists": 5,
+  "followers": 53809,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2026-01-10T23:11:54Z"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +49,9 @@ app.get('/login',(req,res)=>{
 })
   app.get('/youtube',(req,res)=>{
    res.send("this youtube")
+})
+app.get('/detail',(req,res)=>{
+  res.json(githubRepo)
 })
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
